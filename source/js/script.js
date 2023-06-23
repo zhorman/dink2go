b18d43c2b52e39e1c2da7dfa07fbd57d1d641b46
[Georgezho] <[johimichev@gmail.com]>
2023-06-23 13:43:39 +0300

    release

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const menuToggle = document.querySelector('.main-header__toggle');
+const header = document.querySelector('.main-header');
+
+menuToggle.addEventListener('click', () => {
+  header.classList.toggle('main-header--closed');
+  header.classList.toggle('main-header--open');
+});
